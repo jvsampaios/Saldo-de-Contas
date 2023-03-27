fa45a09cbad2a18d001b71bf85081a9087fe8014
João Victor Sampaio <70032581+jvsampaios@users.noreply.github.com>
2023-03-27 14:54:19 -0300

    Update

---
+++ service.js
@@ -0,0 +1,19 @@
+const validarEntradaDeDados = (lancamento) => {
+   return null
+}
+
+const recuperarSaldosPorConta = (lancamentos) => {
+   return []
+}
+
+const recuperarMaiorMenorLancamentos = (cpf, lancamentos) => {
+   return []
+}
+
+const recuperarMaioresSaldos = (lancamentos) => {
+   return []
+}
+
+const recuperarMaioresMedias = (lancamentos) => {
+    return []
+}
